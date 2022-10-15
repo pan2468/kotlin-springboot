a8f0d384b8a6d0aabaafcf0d5af4ebbb0d7d0d38
임채성 <pan1129@naver.com>
2022-10-15 19:39:59 +0900

    조회 테스트 코드 구현 commit

--- src/test/kotlin/com/springboot/service/OrderServiceTest.kt
+++ src/test/kotlin/com/springboot/service/OrderServiceTest.kt
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringRunner
+import org.junit.jupiter.api.Assertions.*
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
@@ -21,9 +22,26 @@ class OrderServiceTest{
     @Test
     @DisplayName("저장 테스트")
     fun orderInsert(){
-        val orders: Order  = Order("user_test", "content_test", "제목" )
+        val orders: Order  = Order("user_test", "title_test", "내용" )
 
         orderRepository.save(orders);
         println(orders.toString())
     }
+
+    @Test
+    @DisplayName("조회 테스트")
+    fun orderList(){
+        this.orderInsert()
+        var userName = "user_test"
+        var title = "title_test"
+        var content = "내용"
+
+        var orderList : List<Order> = orderRepository.findAll();
+        var list : Order  = orderList.get(0)
+
+        assertEquals(list.userName, userName)
+        assertEquals(list.title, title)
+        assertEquals(list.content, content)
+    }
+
 }
