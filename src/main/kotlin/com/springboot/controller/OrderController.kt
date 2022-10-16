c54bea9ea83932afe9f9d6a1847750c8075d6627
임채성 <pan1129@naver.com>
2022-10-16 15:55:52 +0900

    order 등록 구현 commit

--- src/main/kotlin/com/springboot/controller/OrderController.kt
+++ src/main/kotlin/com/springboot/controller/OrderController.kt
@@ -1,6 +1,11 @@
 package com.springboot.controller
 
+import com.springboot.entity.Order
+import com.springboot.service.OrderService
 import lombok.extern.log4j.Log4j2
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,5 +14,12 @@ import org.springframework.web.bind.annotation.RestController
 @Log4j2
 class OrderController {
 
+    @Autowired
+    private lateinit var orderService: OrderService
 
+    @PostMapping("/create")
+    fun orderCreate(@ModelAttribute order: Order){
+        orderService.orderCreate(order)
+    }
 }
+
--- src/main/kotlin/com/springboot/service/OrderService.kt
+++ src/main/kotlin/com/springboot/service/OrderService.kt
@@ -1,10 +1,23 @@
 package com.springboot.service
 
 
+import com.springboot.entity.Order
+import com.springboot.repository.OrderRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
+
+@Transactional
 @Service
-class OrderService(){
+class OrderService{
+
+    @Autowired
+    private lateinit var orderRepository: OrderRepository
+
+    fun orderCreate(order: Order){
+        orderRepository.save(order)
+    }
 
 
 
